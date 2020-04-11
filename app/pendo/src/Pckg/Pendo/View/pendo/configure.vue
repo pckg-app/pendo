1357c874ed1440f303c40ae4698d80c46600bddd
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-04-11 18:16:36 +0200

    Update

--- app/pendo/src/Pckg/Pendo/View/pendo/configure.vue
+++ app/pendo/src/Pckg/Pendo/View/pendo/configure.vue
@@ -38,12 +38,13 @@
 
         </div>
         <div v-else>
-            <p>Country: <b>{{ company.country.code }}</b></p>
+            <!--<p>Country: <b>{{ company.country.code }}</b></p>-->
             <p>Company: <b>{{ company.short_name }}</b></p>
             <p>VAT number: <b>{{ company.vat_number }}</b></p>
+            <p>Business number: <b>{{ company.business_number }}</b></p>
             <p>Contact: <b>{{ user.email }}</b></p>
-            <p>Comms store: <b>{{ appKey.app.id }}</b></p>
-            <p>Certificate: <b>{{ certificate.invisiblePassword }}</b></p>
+            <!--<p>Comms store: <b>{{ appKey.app.id }}</b></p>-->
+            <!--<p>Certificate: <b>{{ certificate.invisiblePassword }}</b></p>-->
             <hr/>
             <button class="btn btn-info" @click.prevent="makeEchoTest">Make echo test</button>
             &nbsp;
